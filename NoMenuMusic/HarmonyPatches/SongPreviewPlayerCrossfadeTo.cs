7e6c6c1b4f97f45ece04effba3dcc816f42c7aed
Aeroluna <aeroluna5@gmail.com>
2020-01-15 13:58:54 -0700

    how do i computer

---
+++ NoMenuMusic/HarmonyPatches/SongPreviewPlayerCrossfadeTo.cs
@@ -0,0 +1,35 @@
+﻿using Harmony;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace NoMenuMusic.HarmonyPatches
+{
+    [HarmonyPatch(typeof(SongPreviewPlayer))]
+    [HarmonyPatch("CrossfadeTo")]
+    [HarmonyPatch(new Type[] { typeof(AudioClip), typeof(float), typeof(float), typeof(float) })]
+    class SongPreviewPlayerCrossfadeTo
+    {
+        static bool Prefix(SongPreviewPlayer __instance, AudioClip audioClip, float startTime, float duration, float volumeScale = 1f)
+        {
+            // Check if this is the default audio clip, and if it is, use the Menu BG Music Volume, otherwise use the preview volume.
+            var defaultClip = ReflectionUtil.GetPrivateField<AudioClip>(__instance, "_defaultAudioClip");
+            if (audioClip == defaultClip)
+            {
+                __instance.volume = 0;
+                Logger.Log("Setting SongPreviewPlayer volume to MenuBGVolume : " + __instance.volume, IPA.Logging.Logger.Level.Debug);
+            }
+            else
+            {
+                __instance.volume = 1;
+                Logger.Log("Setting SongPreviewPlayer volume to PreviewVolume : " + __instance.volume, IPA.Logging.Logger.Level.Debug);
+            }
+
+            // Return true to run the original method as well
+            return true;
+        }
+    }
+}
---
+++ NoMenuMusic/Plugin.cs
@@ -0,0 +1,47 @@
+﻿using IPA;
+using UnityEngine.SceneManagement;
+using Harmony;
+using IPALogger = IPA.Logging.Logger;
+
+namespace NoMenuMusic
+{
+    public class Plugin : IBeatSaberPlugin
+    {
+        internal static HarmonyInstance harmony;
+
+        public void Init(object thisIsNull, IPALogger logger)
+        {
+            Logger.log = logger;
+        }
+
+        public void OnApplicationStart()
+        {
+            harmony = HarmonyInstance.Create("com.aeroluna.BeatSaber.NoMenuMusic");
+            harmony.PatchAll(System.Reflection.Assembly.GetExecutingAssembly());
+        }
+
+        public void OnApplicationQuit()
+        {
+        }
+
+        public void OnFixedUpdate()
+        {
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+        {
+        }
+
+        public void OnSceneLoaded(Scene scene, LoadSceneMode sceneMode)
+        {
+        }
+
+        public void OnSceneUnloaded(Scene scene)
+        {
+        }
+    }
+}
